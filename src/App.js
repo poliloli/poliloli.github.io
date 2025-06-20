ece6b1eb82476600fef28da2c526c3ab0b113529
Luis Augusto <luis.augusto@unilabs.com>
2025-06-20 17:23:46 +0100

    Finished with love hope Filipa likes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
+import React, { useEffect } from 'react';
+import confetti from 'canvas-confetti';
 import './App.css';
 import Wedding from './components/Wedding.js';
 
 function App() {
+  useEffect(() => {
+    const duration = 2 * 1000;
+    const animationEnd = Date.now() + duration;
+
+    const colors = ['#FFB6C1', '#FFC0CB', '#FFF0F5', '#FFE4E1']; // tons florais
+
+    (function frame() {
+      confetti({
+        particleCount: 3,
+        angle: 60,
+        spread: 55,
+        origin: { x: 0 },
+        colors,
+        scalar: 1.2,
+        ticks: 200, 
+        shapes: ['circle'], 
+      });
+
+      confetti({
+        particleCount: 3,
+        angle: 120,
+        spread: 55,
+        origin: { x: 1 },
+        colors,
+        scalar: 1.2,
+        ticks: 200,
+        shapes: ['circle'],
+      });
+
+      if (Date.now() < animationEnd) {
+        requestAnimationFrame(frame);
+      }
+    })();
+  }, []);
   return <Wedding />;
 }
 
--- src/components/Wedding.js
+++ src/components/Wedding.js
@@ -10,141 +10,141 @@ import {
   Link,
   Stack,
   Avatar,
+  useMediaQuery,
 } from "@mui/material";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
 import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
-import bgImage from '../assets/bgImage.jpg';
 import logo from '../assets/logo.png';
-import { Bold } from "lucide-react";
 
 export default function WeddingWelcomePage() {
   const handleAddToCalendar = () => {
-    alert("Evento adicionado ao calendário (mock)");
-    // Aqui podes fazer download de um .ics ou redirecionar para o Google Calendar
+    const link = document.createElement('a');
+    link.href = '/filipa&luis.ics';
+    link.rel = 'noopener noreferrer';
+    document.body.appendChild(link);  // necessário para alguns navegadores
+    link.click();
+    document.body.removeChild(link);
   };
+  const isShort = useMediaQuery('(max-height:760px)');
   return (
     <Box
       sx={{
-        width: '100vw',   // 100% da largura da viewport
-        height: '100vh',  // 100% da altura da viewport
-        overflow: 'auto', // permite scroll se precisar
-        backgroundImage: `url(${bgImage})`,
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
+        height: "100vh",
+        overflow: 'hidden',
         display: "flex",
         flexDirection: "column",
-        justifyContent: "center"
+        alignItems: "center",
+        justifyContent: 'space-between'
       }}
     >
       <Avatar
         alt="Logo"
         src={logo}
         sx={{
-          flex: 1,
-          width: 225,
+          width: isShort ? 125 : 280,
           height: 'auto',
-          alignSelf: "center",
-          marginTop: 5
+          mt: 2
         }}
       />
-      <Box
-        width="100%"
-        height='auto'
+      <Container
+        maxWidth="md"
         sx={{
-          flex: 3,
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          gap: 5
-        }}
-      >
-        <Container sx={{ my: 7, display: 'flex', flexDirection: 'column', gap: 15 }}>
-          <Grid
-            container
-            spacing={5}
-            justifyContent="center"
-            alignItems="stretch"
-            sx={{ display: 'flex', flexDirection: 'column', alignContent: "center" }}
-          >
-            <Grid item xs={12} md={6} sx={{ display: 'flex', flex: 1 }}>
-              <Card sx={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
-                <CardContent sx={{ wordBreak: 'break-word', overflowWrap: 'break-word' }}>
-                  <Stack direction="row" alignItems="center" spacing={1} mb={1}>
-                    <LocationOnIcon color="error" />
-                    <Typography variant="h6" fontWeight="bold" fontFamily="'Cormorant Garamond', serif" color="#503d05">Cerimónia</Typography>
-                  </Stack>
-                  <Stack direction="column" spacing={3} mb={1}>
-                    <Typography variant="body1" fontFamily="'Cormorant Garamond', serif" color="#503d05">
-                      Igreja São Pedro Fins, Largo Souto, São Pedro Fins
-                    </Typography>
-                    <Link
-                      href="https://maps.app.goo.gl/vXSYvWbh6gj4ghhS6"
-                      target="_blank"
-                      rel="noopener"
-                      underline="hover"
-                      fontFamily="'Cormorant Garamond', serif"
-                    >
-                      Ver no mapa
-                    </Link>
-                  </Stack>
-                </CardContent>
-              </Card>
-            </Grid>
-            <Grid item xs={12} md={6} sx={{ display: 'flex', flex: 1 }}>
-              <Card sx={{ flex: 1 }}>
-                <CardContent sx={{ wordBreak: 'break-word', overflowWrap: 'break-word' }}>
-                  <Stack direction="row" alignItems="center" spacing={1} mb={1}>
-                    <LocationOnIcon color="error" />
-                    <Typography
-                      variant="h6"
-                      fontWeight="bold"
-                      fontFamily="'Cormorant Garamond', serif"
-                      color="#503d05">
-                      Festa
-                    </Typography>
-                  </Stack>
-                  <Stack direction="column" spacing={3} mb={1}>
-                    <Typography
-                      variant="body1"
-                      fontFamily="'Cormorant Garamond', serif"
-                      color="#503d05">
-                      Quinta Cardeal, Rua Boaventura Ferreira de Sousa, Fajozes
-                    </Typography>
-                    <Link
-                      href="https://maps.app.goo.gl/NFzjq2ZEhmCxaNyLA"
-                      target="_blank"
-                      rel="noopener"
-                      underline="hover"
-                      fontFamily="'Cormorant Garamond', serif"
-                    >
-                      Ver no mapa
-                    </Link>
-                  </Stack>
-                </CardContent>
-              </Card>
-            </Grid>
-          </Grid>
+          flexGrow: 1,
+          mt: { xs: 3, sm: 7 },
+          gap: isShort ? 5 : 12.5,
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+        }}>
+        <Grid
+          container
+          justifyContent="center"
+          alignItems="stretch"
 
-          <Box textAlign="center" sx="flex: 1">
-            <Button
-              variant="contained"
-              size="large"
-              startIcon={<CalendarMonthIcon />}
-              onClick={handleAddToCalendar}
-              sx={{
-                backgroundColor: '#b4a475',
-                color: '#fff', // ajusta conforme o contraste
-                '&:hover': {
-                  backgroundColor: '#a39267', // versão mais escura no hover
-                },
-              }}
-            >
-              Adicionar ao calendário
-            </Button>
-          </Box>
-        </Container>
-      </Box>
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignContent: "center",
+            gap: { xs: 5, sm: 7 }
+          }}
+        >
+          <Grid item xs={12} md={6} sx={{ display: 'flex', flex: 1 }}>
+            <Card sx={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
+              <CardContent sx={{ wordBreak: 'break-word', overflowWrap: 'break-word' }}>
+                <Stack direction="row" alignItems="center" spacing={1} mb={1}>
+                  <LocationOnIcon color="error" />
+                  <Typography variant="h6" fontWeight="bold" fontFamily="'Cormorant Garamond', serif" color="#503d05">Cerimónia</Typography>
+                </Stack>
+                <Stack direction="column" spacing={3} mb={1}>
+                  <Typography variant="body1" fontFamily="'Cormorant Garamond', serif" color="#503d05">
+                    Igreja São Pedro Fins, Largo Souto, São Pedro Fins
+                  </Typography>
+                  <Link
+                    href="https://maps.app.goo.gl/vXSYvWbh6gj4ghhS6"
+                    target="_blank"
+                    rel="noopener"
+                    underline="none"
+                    fontFamily="'Cormorant Garamond', serif"
+                  >
+                    Ver no mapa
+                  </Link>
+                </Stack>
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid item xs={12} md={6} sx={{ display: 'flex', flex: 1 }}>
+            <Card sx={{ flex: 1 }}>
+              <CardContent sx={{ wordBreak: 'break-word', overflowWrap: 'break-word' }}>
+                <Stack direction="row" alignItems="center" spacing={1} mb={1}>
+                  <LocationOnIcon color="error" />
+                  <Typography
+                    variant="h6"
+                    fontWeight="bold"
+                    fontFamily="'Cormorant Garamond', serif"
+                    color="#503d05">
+                    Depois da Cerimónia
+                  </Typography>
+                </Stack>
+                <Stack direction="column" spacing={3} mb={1}>
+                  <Typography
+                    variant="body1"
+                    fontFamily="'Cormorant Garamond', serif"
+                    color="#503d05">
+                    Quinta Cardeal, Rua Boaventura Ferreira de Sousa, Fajozes
+                  </Typography>
+                  <Link
+                    href="https://maps.app.goo.gl/NFzjq2ZEhmCxaNyLA"
+                    target="_blank"
+                    rel="noopener"
+                    underline="none"
+                    fontFamily="'Cormorant Garamond', serif"
+                  >
+                    Ver no mapa
+                  </Link>
+                </Stack>
+              </CardContent>
+            </Card>
+          </Grid>
+        </Grid>
+
+        <Box textAlign="center">
+          <Button
+            variant="contained"
+            size="large"
+            startIcon={<CalendarMonthIcon />}
+            onClick={handleAddToCalendar}
+            sx={{
+              backgroundColor: '#b4a475',
+              color: '#fff', // ajusta conforme o contraste
+              '&:hover': {
+                backgroundColor: '#a39267', // versão mais escura no hover
+              },
+            }}
+          >
+            Adicionar ao calendário
+          </Button>
+        </Box>
+      </Container>
     </Box>
   );
 }
